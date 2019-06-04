d92b949d8190a6e3ec15b91cd380d78558ecc29b
Paul Statezny <paulstatezny@gmail.com>
2019-06-03 19:17:40 -0700

    Complete chapter 8 exercises

--- 8_recursion/exercises.hs
+++ 8_recursion/exercises.hs
@@ -30,3 +30,29 @@ recursiveMultiply a b
 data DividedResult =
     Result Integer
     | DividedByZero
+    deriving (Show)
+
+
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+  where go n d count
+         | n < d = (count, n)
+         | otherwise =
+             go (n - d) d (count + 1)
+
+
+-- Using DividedResult
+
+
+dividedBy' :: Integral a => a -> a -> DividedResult
+dividedBy' num denom = go num denom 0
+  where go n d count
+         | d == 0 = DividedByZero
+         | n < d = Result count
+         | otherwise =
+             go (n - d) d (count + 1)
+
+mc91 :: Integral a => a -> a
+mc91 a
+  | a > 100   = a - 10
+  | otherwise = mc91 $ mc91 $ a + 11
